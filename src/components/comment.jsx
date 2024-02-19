e165f78a6545c523b65c67c3aee0bd520661deb5
llleeeaaannn <piercefrank10@gmail.com>
2024-02-19 23:46:30 +0000

    Added component files

---
+++ src/components/comment.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Comment = () => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Comment;
---
+++ src/components/post.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Post = () => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Post;
